46168c5329db9182db683b7a306dd76a2f024df1
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-12-05 16:01:35 +0300

    add docstrings

--- src/json_utils.py
+++ src/json_utils.py
@@ -8,6 +8,7 @@ from .abs_classes import FileHandler
 class JSONSaver(FileHandler):
 
     def __init__(self):
+        """Инициализирует путь к файлу с вакансиями"""
         today = datetime.today().strftime("%d.%m.%Y")
         filename = f"{today}.json"
 
@@ -15,6 +16,7 @@ class JSONSaver(FileHandler):
         self.__path = path
 
     def add_vacancies(self, vacancies: list):
+        """Добавляет вакансии в файл с вакансиями"""
         with open(self.__path, "a+") as file:
             data = file.read()
             if data:
@@ -25,6 +27,7 @@ class JSONSaver(FileHandler):
             json.dump(data, file, ensure_ascii=False, indent=4)
 
     def get_vacancies_by_salary(self, salary: int):
+        """Получет все вакансии с заданной зарплатой"""
         try:
             with open(self.__path) as file:
                 vacancies = json.load(file)
@@ -36,6 +39,7 @@ class JSONSaver(FileHandler):
             raise FileNotFoundError("Файл с вакансиями не найден")
 
     def delete_vacancy(self, vacancy: dict):
+        """Удаляет вакансию из файла с вакансиями"""
         try:
             with open(self.__path) as file:
                 vacancies = json.load(file)
